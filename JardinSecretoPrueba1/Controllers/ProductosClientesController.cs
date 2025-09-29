fcd8961d338869d770480aa02bce5674bc148c56
Jaime Marro <jaime28marro@gmail.com>
2025-09-29 12:19:48 -0600

    Se añadio y quizas se corrigio errores al abrir

---
+++ JardinSecretoPrueba1/Controllers/ProductosClientesController.cs
@@ -0,0 +1,70 @@
+﻿using JardinSecretoPrueba1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JardinSecretoPrueba1.Controllers
+{
+    public class ProductosClientesController : Controller
+    {
+        private readonly JardinSecretoContext _context;
+
+        public ProductosClientesController(JardinSecretoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: muestra el formulario
+        [HttpGet]
+        public IActionResult NombreUsuario()
+        {
+            return View();
+        }
+
+        // POST: recibe datos del formulario
+        [HttpPost]
+        public IActionResult NombreUsuario(string nombreusuario, string tipoPedido)
+        {
+            HttpContext.Session.SetString("NombreUsuario", nombreusuario);
+            HttpContext.Session.SetString("TipoPedido", tipoPedido);
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        public async Task<IActionResult> Index()
+        {
+            var nombre = HttpContext.Session.GetString("NombreUsuario") ?? "Invitado";
+            var pedido = HttpContext.Session.GetString("TipoPedido") ?? "No especificado";
+
+            ViewBag.Nombre = nombre;
+            ViewBag.TipoPedido = pedido;
+
+            var jardinSecretoContext = _context.Productos.Include(p => p.Categoria);
+            return View(await jardinSecretoContext.ToListAsync());
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var producto = await _context.Productos
+                .Include(p => p.Categoria)
+                .FirstOrDefaultAsync(m => m.ProductoId == id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+
+        //Configuracion del carrito Carrito
+        public IActionResult Carrito()
+        {
+            return View();
+        }
+    }
+}
---
+++ JardinSecretoPrueba1/Models/Extra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JardinSecretoPrueba1.Models;
+
+public partial class Extra
+{
+    public int ExtraId { get; set; }
+
+
+    public int IdProducto { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public decimal PrecioExtra { get; set; }
+
+    public virtual Producto? IdProductoNavigation { get; set; }
+}
